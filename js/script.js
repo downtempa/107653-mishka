473479222ba453307b15f175680a85b3b4039896
Leonid Mistyukov <downtempa@mail.ru>
2018-01-15 00:16:39 +0300

    menu toggle style & logic, mobile menu layout

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// main menu toggle
+var menuToggle = document.querySelector('#main-menu-toggle');
+
+menuToggle.onclick = function() {
+  this.classList.toggle('js-active');
+}
+
+console.log(menuToggle);
