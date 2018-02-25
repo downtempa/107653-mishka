79d26560cf15e0b4bed2ce251c58771e5fcfba52
Leonid Mistyukov <downtempa@mail.ru>
2018-02-25 16:43:21 +0300

    footer done

--- js/script.js
+++ js/script.js
@@ -23,3 +23,36 @@ var slider = tns({
   mouseDrag: true,
   controlsContainer: document.querySelector('.slider__control')
 });
+
+ymaps.ready(function () {
+  ymaps.ready(init);
+  var myMap,  myPlacemark;
+
+  function init(){
+    myMap = new ymaps.Map("map", {
+      center: [59.93867682, 30.32309742],
+      zoom: 18,
+      controls: []
+    });
+
+
+    myPlacemark = new ymaps.Placemark([59.93867682, 30.32309742], {
+      hintContent: 'mishka.ru',
+      balloonContent: 'mishka.ru'
+    }, {
+      // Опции.
+      // Необходимо указать данный тип макета.
+      iconLayout: 'default#image',
+      // Своё изображение иконки метки.
+      iconImageHref: 'img/icon-map-pin.svg',
+      // Размеры метки.
+      iconImageSize: [66, 101],
+      // Смещение левого верхнего угла иконки относительно
+      // её "ножки" (точки привязки).
+      iconImageOffset: [-30, -100]
+    });
+
+    myMap.geoObjects.add(myPlacemark);
+  }
+
+});
